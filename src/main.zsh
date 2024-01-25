87e19f91dec5f97c9d3b97ae6a8e111dda5d93fd
wuchuheng <root@wuchuheng.com>
2024-01-25 19:38:24 +0800

    feat: Enhance Git Plugin Functionality and Integrate with ZPM
    - Added a new `.gitkeep` file under `src/custom/plugins` for better directory structure management.
    - Created `README.md` for the Git plugin, providing detailed documentation on usage, aliases, and functions.
    - Implemented `git.plugin.zsh` script with extensive Git aliases and utility functions.
    - Modified `main.zsh` to include a new function `load_custom_plugins` for dynamically loading custom plugins from the specified directory.
    - Enabled command line completion and set menu style for enhanced user interaction.
    
    This update significantly expands the Git plugin capabilities within the ZPM environment, offering a comprehensive set of tools and aliases for efficient Git operations. Additionally, the dynamic loading of custom plugins enhances flexibility and customization for users.

--- src/main.zsh
+++ src/main.zsh
@@ -1,9 +1,29 @@
 #!/usr/bin/env zpm
+autoload -Uz compinit && compinit
+zstyle ':completion:*' menu select
 
 function init() {
     call main
 }
 
 function main() {
-    echo "Hello world: Dotfiles"
+    call load_custom_plugins
+}
+
+function load_custom_plugins() {
+    local allPlugins=()
+    for plugin in ${ZPM_WORKSPACE}/src/custom/plugins/*; do
+        if [[ -d $plugin ]]; then
+            local pluginName=${plugin##*/}
+            local entryFile=$plugin/${pluginName}.plugin.zsh
+            if [[ -f $entryFile ]]; then
+                allPlugins+=($entryFile)
+            fi
+        fi
+    done
+
+    # load all plugins
+    for plugin in ${allPlugins[@]}; do
+        source $plugin
+    done
 }
