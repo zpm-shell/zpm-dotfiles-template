61b58772fe60421bdb651959e21427111360e1ad
wuchuheng <root@wuchuheng.com>
2024-01-26 22:10:42 +0800

    feat: Refactor custom plugin loading and remove git plugin
    - Implemented a new approach for loading custom plugins in `custom_plugin.zsh` using `jq` for JSON parsing.
    - Custom plugins are now loaded based on the `customPlugins` field in `zpm-package.json`.
    - Enhanced error handling and validation for plugin dependencies and entry files.
    - Removed the git plugin and associated files (`git.plugin.zsh`, `README.md`, `.gitkeep`) from `src/custom/plugins/git`, reflecting a shift in plugin management strategy.
    - Updated `zpm-package.json` to reflect these changes, including the addition of `github.com/zsh-users/zsh-syntax-highlighting` as a custom plugin.

--- src/providers/custom_plugin.zsh
+++ src/providers/custom_plugin.zsh
@@ -1,19 +1,50 @@
 #!/usr/bin/env zsh
 
+import github.com/zpm-shell/zpm/src/utils/bin.zsh --as bin
+
 function load() {
-    local allPlugins=()
-    for plugin in ${ZPM_WORKSPACE}/src/custom/plugins/*; do
-        if [[ -d $plugin ]]; then
-            local pluginName=${plugin##*/}
-            local entryFile=$plugin/${pluginName}.plugin.zsh
-            if [[ -f $entryFile ]]; then
-                allPlugins+=($entryFile)
-            fi
-        fi
+    local jq=$( call bin.jq )
+
+    # 1 get the plugin names from zpm-package.json with the field `customPlugins`.
+    local plugins=()
+    local packageData="$( cat ${G_DOTFILES_ROOT}/zpm-package.json )"
+    local hasPlugin=$( $jq -q customPlugins -j "${packageData}" -t has )
+    [[ ${hasPlugin} == 'false' ]] && return ${TRUE}
+    local size=$( $jq -q customPlugins -j "${packageData}" -t size )
+    [[ ${size} -eq 0 ]] && return ${TRUE}
+    echo  "size: ${size}"
+    local i=0;
+    while [[ $i -lt ${size} ]]; do
+        local plugin=$( $jq -q customPlugins.$i -j "${packageData}" -t get )
+        plugins+=( "${plugin}" )
+        (( i++ ))
     done
 
-    # load all plugins
-    for plugin in ${allPlugins[@]}; do
-        source $plugin
+    # 2 load the plugins
+    i=1
+    while [[ $i -le ${#plugins[@]} ]]; do
+        # 2.1 check the plugin is installed or not.
+        local plugin=${plugins[$i]}
+        local query="dependencies.${plugin//\./\\.}"
+        local hasDependence=$( $jq -q ${query} -j "${packageData}" -t has)
+        if [[ ${hasDependence} == 'false' ]]; then
+            throw \
+            --error-message "the plugin ${plugin} was not found.please try the cmd: zpm install ${plugin} to install." \
+            --exit-code 1
+        fi
+
+        # 2.2 get the version of the plugin.
+        local version=$( $jq -q ${query} -j "${packageData}" -t get )
+        # 2.3 get the entry file name of the plugin.like: get the name zsh-syntax-highlighting in path/to/zsh-syntax-highlighting
+        local entryFile=${ZPM_DIR}/packages/${plugin}/${version}/${plugin##*/}.plugin.zsh
+        if [[ -f ${entryFile} ]]; then
+            source ${entryFile}
+        else
+            throw \
+            --error-message "the entry file ${entryFile} was not found for the plugin ${plugin}." \
+            --exit-code 1
+        fi
+
+        (( i++ ))
     done
 }
