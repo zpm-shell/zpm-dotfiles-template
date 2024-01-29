f5985485baec36458c48df12081b27a8e9f0bd73
wuchuheng <root@wuchuheng.com>
2024-01-29 16:40:58 +0800

    feat:Introduce New Library Function and Update Library Loading Mechanism
    - Added a new library function `lib-demo` in `src/lib/lib-demo.zsh`:
       - The function `lib-demo` outputs "lib-demo" when called.
    
    - Revised the library loading process in `src/providers/lib.zsh`:
       - Uncommented and activated the library loading code.
       - Implemented a directory check to ensure libraries are present before attempting to load.
       - The updated script now iterates over and sources each `.zsh` file in the `src/lib` directory.
    
    These changes introduce a new library function to enhance the system's capabilities and update the library loading mechanism for improved reliability and organization.

---
+++ src/lib/lib-demo.zsh
@@ -0,0 +1,3 @@
+function lib-demo() {
+    echo "lib-demo"
+}
--- src/providers/lib.zsh
+++ src/providers/lib.zsh
@@ -2,9 +2,12 @@
 
 function load() {
     local lib=''
-    # for lib in ${G_DOTFILES_ROOT}/src/lib/*.zsh; do
-    #     if [[ -f ${lib} ]]; then
-    #         source "${lib}"
-    #     fi
-    # done
+    local libPath="${G_DOTFILES_ROOT}/src/lib"
+    if [[ -n "$(ls -A ${libPath})" ]]; then
+        for lib in ${libPath}/*.zsh; do
+            if [[ -f ${lib} ]]; then
+                source "${lib}"
+            fi
+        done
+    fi
 }
